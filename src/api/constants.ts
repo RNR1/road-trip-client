ef1d3bff3d499d1f9d94fac36a873b55dbee970f
Ron Braha <ron.braha@icloud.com>
2021-05-28 18:22:52 +0300

    fix: api_url

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,2 +1,2 @@
-export const baseURL = process.env.VITE_API_URL;
+export const baseURL = process.env.VITE_API_URL ?? import.meta.env.VITE_API_URL;
 export const defaultHeaders = { 'Content-Type': 'application/json' };
